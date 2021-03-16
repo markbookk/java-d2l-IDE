8d1c21bf208f736555cc181614bd6f9bca8f9d7e
markbookk <marcos.nieves1@upr.edu>
2021-03-16 14:07:25 -0400

    Formatting code and adding more descritive doc to a function

--- section8_5/src/main/java/Training.java
+++ section8_5/src/main/java/Training.java
@@ -21,8 +21,9 @@ class Training {
     }
 
     public static NDArray squaredLoss(NDArray yHat, NDArray y) {
-        return (yHat.sub(y.reshape(yHat.getShape()))).mul
-                ((yHat.sub(y.reshape(yHat.getShape())))).div(2);
+        return (yHat.sub(y.reshape(yHat.getShape())))
+                .mul((yHat.sub(y.reshape(yHat.getShape()))))
+                .div(2);
     }
 
     public static void sgd(NDList params, float lr, int batchSize) {
@@ -34,7 +35,13 @@ class Training {
         }
     }
 
-    /** Allow to do gradient calculations on subManager **/
+    /**
+     * Allows to do gradient calculations on a subManager. This is very useful when you are training
+     * on a lot of epochs. This subManager could later be closed and all NDArrays generated from the
+     * calculations in this function will be cleared from memory when subManager is closed. This is
+     * always a great practice but the impact is most notable when there is lot of data on various
+     * epochs.
+     */
     public static void sgd(NDList params, float lr, int batchSize, NDManager subManager) {
         for (int i = 0; i < params.size(); i++) {
             NDArray param = params.get(i);
@@ -53,15 +60,28 @@ class Training {
             // Argmax gets index of maximum args for given axis 1
             // Convert yHat to same dataType as y (int32)
             // Sum up number of true entries
-            return yHat.argMax(1).toType(DataType.INT32, false).eq(y.toType(DataType.INT32, false))
-                    .sum().toType(DataType.FLOAT32, false).getFloat();
+            return yHat.argMax(1)
+                    .toType(DataType.INT32, false)
+                    .eq(y.toType(DataType.INT32, false))
+                    .sum()
+                    .toType(DataType.FLOAT32, false)
+                    .getFloat();
         }
-        return yHat.toType(DataType.INT32, false).eq(y.toType(DataType.INT32, false))
-                .sum().toType(DataType.FLOAT32, false).getFloat();
+        return yHat.toType(DataType.INT32, false)
+                .eq(y.toType(DataType.INT32, false))
+                .sum()
+                .toType(DataType.FLOAT32, false)
+                .getFloat();
     }
 
-    public static void trainingChapter6(ArrayDataset trainIter, ArrayDataset testIter,
-                                        int numEpochs, Trainer trainer, Map<String, double[]> evaluatorMetrics, double avgTrainTimePerEpoch) throws IOException, TranslateException {
+    public static void trainingChapter6(
+            ArrayDataset trainIter,
+            ArrayDataset testIter,
+            int numEpochs,
+            Trainer trainer,
+            Map<String, double[]> evaluatorMetrics,
+            double avgTrainTimePerEpoch)
+            throws IOException, TranslateException {
 
         trainer.setMetrics(new Metrics());
 
@@ -70,23 +90,32 @@ class Training {
         Metrics metrics = trainer.getMetrics();
 
         trainer.getEvaluators().stream()
-                .forEach(evaluator -> {
-                    evaluatorMetrics.put("train_epoch_" + evaluator.getName(), metrics.getMetric("train_epoch_" + evaluator.getName()).stream()
-                            .mapToDouble(x -> x.getValue().doubleValue()).toArray());
-                    evaluatorMetrics.put("validate_epoch_" + evaluator.getName(), metrics.getMetric("validate_epoch_" + evaluator.getName()).stream()
-                            .mapToDouble(x -> x.getValue().doubleValue()).toArray());
-                });
+                .forEach(
+                        evaluator -> {
+                            evaluatorMetrics.put(
+                                    "train_epoch_" + evaluator.getName(),
+                                    metrics.getMetric("train_epoch_" + evaluator.getName()).stream()
+                                            .mapToDouble(x -> x.getValue().doubleValue())
+                                            .toArray());
+                            evaluatorMetrics.put(
+                                    "validate_epoch_" + evaluator.getName(),
+                                    metrics
+                                            .getMetric("validate_epoch_" + evaluator.getName())
+                                            .stream()
+                                            .mapToDouble(x -> x.getValue().doubleValue())
+                                            .toArray());
+                        });
 
         avgTrainTimePerEpoch = metrics.mean("epoch");
     }
 
     /* Softmax-regression-scratch */
     public static float evaluateAccuracy(UnaryOperator<NDArray> net, Iterable<Batch> dataIterator) {
-        Accumulator metric = new Accumulator(2);  // numCorrectedExamples, numExamples
+        Accumulator metric = new Accumulator(2); // numCorrectedExamples, numExamples
         for (Batch batch : dataIterator) {
             NDArray X = batch.getData().head();
             NDArray y = batch.getLabels().head();
-            metric.add(new float[]{accuracy(net.apply(X), y), (float)y.size()});
+            metric.add(new float[] {accuracy(net.apply(X), y), (float) y.size()});
             batch.close();
         }
         return metric.get(0) / metric.get(1);
@@ -95,13 +124,17 @@ class Training {
 
     /* MLP */
     /* Evaluate the loss of a model on the given dataset */
-    public static float evaluateLoss(UnaryOperator<NDArray> net, Iterable<Batch> dataIterator, BinaryOperator<NDArray> loss) {
-        Accumulator metric = new Accumulator(2);  // sumLoss, numExamples
+    public static float evaluateLoss(
+            UnaryOperator<NDArray> net,
+            Iterable<Batch> dataIterator,
+            BinaryOperator<NDArray> loss) {
+        Accumulator metric = new Accumulator(2); // sumLoss, numExamples
 
         for (Batch batch : dataIterator) {
             NDArray X = batch.getData().head();
             NDArray y = batch.getLabels().head();
-            metric.add(new float[]{loss.apply(net.apply(X), y).sum().getFloat(), (float) y.size()});
+            metric.add(
+                    new float[] {loss.apply(net.apply(X), y).sum().getFloat(), (float) y.size()});
             batch.close();
         }
         return metric.get(0) / metric.get(1);
